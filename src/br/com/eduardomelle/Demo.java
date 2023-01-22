b4005cf2c2399e4e98d1ccda06ae2682ced45096
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2023-01-22 09:54:11 -0300

    O que são Virtual Threads e porque você deveria se importar com isso

--- src/br/com/eduardomelle/Demo.java
+++ src/br/com/eduardomelle/Demo.java
@@ -3,6 +3,8 @@
  */
 package br.com.eduardomelle;
 
+import java.util.concurrent.Executors;
+
 /**
  * @author eduardo
  *
@@ -22,6 +24,25 @@ public class Demo {
 			System.out.println("HELLO, THREAD!");
 		});
 		thread.join();
+	}
+
+	void runExecutor() throws Exception {
+		for (;;) {
+			long start = System.currentTimeMillis();
+
+			try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+				for (int i = 0; i < 100_000; i++) {
+					executor.submit(() -> {
+						Thread.sleep(2000);
+						return null;
+					});
+				}
+			}
+
+			long end = System.currentTimeMillis();
+
+			System.out.println(start - end + " ms");
+		}
 	}
 
 }
